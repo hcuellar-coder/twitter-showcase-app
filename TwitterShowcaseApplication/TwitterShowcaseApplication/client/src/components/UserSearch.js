fadd27d5ed61f3d625cff6c483297b79092d442d
Eddie Cuellar <heriberto.cuellar1329@gmail.com>
2020-10-01 23:30:45 -0500

    Added a search bar in User Search

--- TwitterShowcaseApplication/TwitterShowcaseApplication/client/src/components/UserSearch.js
+++ TwitterShowcaseApplication/TwitterShowcaseApplication/client/src/components/UserSearch.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Form, FormControl, Button } from 'react-bootstrap';
 
 function UserSearch() {
     return (
-        <div></div>
+        <div>
+            <Form inline>
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                <Button variant="outline-success">Search</Button>
+            </Form>
+        </div>
     )
 }
 
